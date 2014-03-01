c9d345108802a3098f29f7446d3456e937ac58e6
Joe G <wheeltirefamily@gmail.com>
2014-03-01 12:13:11 -0600

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qrjoe::Application.config.secret_key_base = 'f3b47539f400cfa616c8915c2b29b18aecae73da0b921110ba7adff8c6dcab83fd3f846e8ebf883b75bd2fe5456082aafffd4e1bb53a85b979a6cba290a7f259'
